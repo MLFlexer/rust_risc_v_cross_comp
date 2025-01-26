cbbab3b8232a1a72b63095656ce10c7f1a2f053b
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-01-26 18:29:51 +0100

    update qemu

--- flake.nix
+++ flake.nix
@@ -3,26 +3,30 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, rust-overlay, nixpkgs, }: {
-    devShells.x86_64-linux.default = let
-      pkgsCross = nixpkgs.legacyPackages.x86_64-linux.pkgsCross.riscv64;
-      rust-bin = rust-overlay.lib.mkRustBin { } pkgsCross.buildPackages;
-    in pkgsCross.callPackage ({ mkShell, pkg-config, qemu, openssl, stdenv, }:
-      mkShell {
-        nativeBuildInputs = [ rust-bin.stable.latest.minimal pkg-config ];
+  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgsCross = nixpkgs.legacyPackages.${system}.pkgsCross.riscv64;
+        rust-bin = rust-overlay.lib.mkRustBin { } pkgsCross.buildPackages;
+        pkgs = import nixpkgs { system = "${system}"; };
+      in {
+        devShells = {
+          default = pkgsCross.mkShell {
+            nativeBuildInputs =
+              [ rust-bin.stable.latest.minimal pkgsCross.pkg-config ];
 
-        depsBuildBuild = [ qemu ];
-        # buildInputs = [ openssl ];
+            depsBuildBuild = [ pkgs.qemu ];
 
-        env = {
-          CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_GNU_LINKER =
-            "${stdenv.cc.targetPrefix}cc";
-          CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_GNU_RUNNER =
-            "qemu-riscv64"; # Running in emulator
+            env = {
+              CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_GNU_LINKER =
+                "${pkgsCross.stdenv.cc.targetPrefix}cc";
+              CARGO_TARGET_RISCV64GC_UNKNOWN_LINUX_GNU_RUNNER =
+                "qemu-riscv64"; # Running in emulator
+            };
+          };
         };
-
-      }) { };
-  };
+      });
 }
