76c9aed4b85b41766627a3dc74a33577246afb8d
Malthe Larsen <75012728+MLFlexer@users.noreply.github.com>
2025-02-09 12:33:03 +0100

    Update c/start.s

--- c/start.s
+++ c/start.s
@@ -6,8 +6,7 @@
 _start:
   la sp, __stack_top # load address (la) of the stack (__stack_top) into the stack pointer (sp)
   add s0, sp, zero # set the frame pointer to the top of the stack (sp) by setting s0 = sp + zero -> s0 = __stack_top + 0
-  jal zero, main # Jump and link (jal) TODO: remove
-  # jal main # Jump and link (jal) to main, aka. call main()
+  jal zero, main # Jump and link (jal)
 loop: j loop # loop forever if we return from main()
 
 # Initialize the stack
