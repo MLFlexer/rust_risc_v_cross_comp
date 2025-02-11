e71d40bb7ff8a9326f7538a631e910ebb311df90
MLFlexer <75012728+MLFlexer@users.noreply.github.com>
2025-02-11 11:36:25 +0100

    add macos opt

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
           pkgsCross =
             nixpkgs.legacyPackages.${system}.pkgsCross.riscv64-embedded;
           rust-bin = rust-overlay.lib.mkRustBin { } pkgsCross.buildPackages;
+          is_macOS = system == "x86_64-darwin" || system == "aarch64-darwin";
         in pkgsCross.callPackage ({ mkShell, pkg-config, qemu, stdenv }:
           mkShell {
             nativeBuildInputs = [ rust-bin.stable.latest.minimal pkg-config ];
@@ -26,6 +27,8 @@
                 "${pkgsCross.buildPackages.gdb}/bin/riscv64-none-elf-gdb";
               LIBCLANG_PATH =
                 "${pkgsCross.buildPackages.llvmPackages.libclang.lib}/lib";
+              LIBRARY_PATH =
+                if is_macOS then "$(xcrun --show-sdk-path)/usr/lib" else "";
             };
 
           }) { };
